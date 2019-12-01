af6f74baec40627c5d755bbf06f333970ccc3bc8
Carlos Diaz Lopez <ryankar90@hotmail.com>
2019-12-01 16:42:31 -0600

    add modified standart to net framework

---
+++ ServiceKYA/ViewModels/Response/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels.Response
+{
+    public class Response<T>
+    {
+        public T Result { get; set; }
+        public int Count { get; set; }
+        public string Message { get; set; }
+    }
+}
